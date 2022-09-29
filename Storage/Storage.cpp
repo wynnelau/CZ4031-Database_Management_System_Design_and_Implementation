9f90b08da147d2b7debaa5cb311f228b40240b91
jeromechew <jeromechew15@gmail.com>
2022-09-29 19:16:07 +0800

    Storage Changes

--- Storage/Storage.cpp
+++ Storage/Storage.cpp
@@ -3,11 +3,45 @@
 //
 
 #include "Storage.h"
+#include "Block.h"
+#include <vector>
 
 using namespace std;
 
 Storage::Storage(size_t memorySize, size_t blockSize) {
-    this->memorySize = memorySize;
-    this->blockSize = blockSize;
-    this->blocks = operator new(memorySize);
+    memorySize = memorySize;
+    blockSize = blockSize;
+    maxBlocks = memorySize / blockSize;
+    curBlocks = 0;
+    vector<Block> blocks = vector<Block> (maxBlocks);
 }
+
+bool Storage::insertRecord(Record record){
+    // Storage is full
+    for(int i =1; i<=curBlocks; i++){
+        // Checking if blocks have available spaces (including deleted spaces)
+        if (blocks.at(i-1).getAvailableSpace()){
+        blocks.at(i-1).insertRecord(record);
+        return true;
+    }
+
+    if(curBlocks <= maxBlocks){
+        blocks.at(curBlocks) = Block(blockSize);
+        blocks.at(curBlocks).insertRecord(record);
+        curBlocks++;
+        return true;
+    }
+
+    else{
+        cout << "Storage is full!" << endl;
+        return false;
+    }
+}
+}
+
+bool Storage::deleteRecord(Address address){
+    if(address.getBlockNumber() > maxBlocks) return false;
+    blocks.at(address.getBlockNumber() -1 ).deleteRecord(address.getOffSet());
+    return true;
+}
+
--- Storage/Storage.h
+++ Storage/Storage.h
@@ -6,6 +6,7 @@
 #define STORAGE_H
 #include "Record.h"
 #include "Block.h"
+#include "Address.h"
 #include <iostream>
 #include <vector>
 
@@ -15,10 +16,14 @@ using namespace std;
 class Storage {
     int blockSize;
     int memorySize;
+    int maxBlocks;
+    int curBlocks;
     vector<Block> blocks;
 
 public:
     Storage(size_t memorySize, size_t blockSize);
+    bool insertRecord(Record record);
+    bool deleteRecord(Address address);
 };
 
 
