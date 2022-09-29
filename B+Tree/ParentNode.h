be84b32a0eae4b5f103991d36e50d7ac2d04c2fe
akshitkaranam <78993046+akshitkaranam@users.noreply.github.com>
2022-09-29 14:09:30 +0800

    changes to node structure

---
+++ B+Tree/ParentNode.h
@@ -0,0 +1,14 @@
+//
+// Created by Akshit Karanam on 29/9/22.
+//
+
+#ifndef CZ4031_PROJECT1_PARENTNODE_H
+#define CZ4031_PROJECT1_PARENTNODE_H
+
+
+class ParentNode {
+
+};
+
+
+#endif //CZ4031_PROJECT1_PARENTNODE_H
